72d32a62c90eab59a8d8d996b55654f0ba3b65cd
Tadashi Ito <10068148+itot555@users.noreply.github.com>
2023-09-22 20:09:12 +0900

    add s3_bucket resources

--- main.tf
+++ main.tf
@@ -16,10 +16,31 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example
+  ]
 }
 
 resource "aws_s3_bucket_policy" "policy" {
@@ -42,6 +63,11 @@ resource "aws_s3_bucket_policy" "policy" {
     ]
 }
 EOF
+
+  depends_on = [
+    aws_s3_bucket.bucket,
+    aws_s3_bucket_acl.bucket
+  ]
 }
 
 resource "aws_s3_object" "webapp" {
@@ -50,4 +76,8 @@ resource "aws_s3_object" "webapp" {
   bucket       = aws_s3_bucket.bucket.id
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
+
+  depends_on = [
+    aws_s3_bucket_policy.policy
+  ]
 }
